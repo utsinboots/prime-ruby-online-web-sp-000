4f6b62c527c2a835c0bb6f3e1b67e94d78ed2605
Utshant Gurung <utshantgurung@gmail.com>
2021-06-07 09:32:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+  if number == 2
+    return true
+  elsif number > 2 
+   i = 2 
+   array = []
+   while i < number
+     array << i
+     i += 1
+   end
+   array.all? {|check| number%check != 0}
+  else 
+   return false
+  end
+ end
